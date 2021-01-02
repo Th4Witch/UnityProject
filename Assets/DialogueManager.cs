30212292bddee32ce561ac753d3c92d68b72c3d7
Th4Witch <66133968+Th4Witch@users.noreply.github.com>
2021-01-03 00:55:44 +0100

    implementation de tout le dialogue

--- Assets/DialogueManager.cs
+++ Assets/DialogueManager.cs
@@ -5,7 +5,10 @@ using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
-    public NPCMonsieurLapin npc;
+    public List<NPCMonsieurLapin> npcs;
+    int state = 0;
+    bool Alice = false;
+
 
     bool isTalking = false;
 
@@ -24,85 +27,119 @@ public class DialogueManager : MonoBehaviour
     void Start()
     {
         dialogueUI.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-      void OnMouseOver()
-      {
-            distance = Vector3.Distance(player.transform.position, this.transform.position);
-            if(distance <= 2.5f)
+    void Update()
+    {
+       
+        
+        NPCMonsieurLapin npc = npcs[state];
+
+        distance = Vector3.Distance(player.transform.position, this.transform.position);
+        if (distance <= 2.5f)
+        {
+            
+            //trigger dialogue
+            if (Input.GetKeyDown(KeyCode.E) && isTalking == false)
             {
-                if(Input.GetAxis("Mouse ScrollWheel") < 0f)
+                StartConversation();
+            }
+            else if (Input.GetKeyDown(KeyCode.E) && isTalking == true)
+            {
+                EndDialogue();
+            }
+            if (!isTalking) return;
+
+            if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+            {
+                curResponseTracker++;
+                if (curResponseTracker >= npc.playerDialogue.Length - 1)
                 {
-                     curResponseTracker++;
-                     if (curResponseTracker >= npc.playerDialogue.Length - 1)
-                     {
-                        curResponseTracker = npc.playerDialogue.Length - 1;
-                     }
+                    curResponseTracker = npc.playerDialogue.Length - 1;
+                }
+            }
+
+            else if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+            {
+                curResponseTracker--;
+                if (curResponseTracker < 0)
+                {
+                    curResponseTracker = 0;
                 }
 
-                else if(Input.GetAxis("Mouse ScrollWheel") > 0f)
+            }
+            if (curResponseTracker == 0 && npc.playerDialogue.Length >= 0)
+            {
+                playerResponse.text = npc.playerDialogue[0];
+                if (Input.GetKeyDown(KeyCode.Return))
                 {
-                   curResponseTracker--;
-                   if(curResponseTracker < 0)
-                   {
-                      curResponseTracker = 0;
-                   }
-
-                }
-                //trigger dialogue
-                if(Input.GetKeyDown(KeyCode.E) && isTalking==false)
-                {
-                   StartConversation();
-                }
-                else if(Input.GetKeyDown(KeyCode.E) && isTalking == true)
-                {
-                   EndDialogue();
-                }
-
-                if(curResponseTracker == 0 && npc.playerDialogue.Length >= 0)
-                {
-                   playerResponse.text = npc.playerDialogue[0];
-                   if(Input.GetKeyDown(KeyCode.Return))
-                   {
-                      npcDialogueBox.text = npc.dialogue[1];
-                   }
-
-                } 
-                else if(curResponseTracker == 1 && npc.playerDialogue.Length >= 1)
-                {
-                    playerResponse.text = npc.playerDialogue[1];
-                    if(Input.GetKeyDown(KeyCode.Return))
+                    
+                    int dialogue = Alice ? (npc.dialogue.Length/2) + 1 : 1;
+                    npcDialogueBox.text = npc.dialogue[dialogue];
+                    if (state == 1) Alice = true;
+                    if (state < npcs.Count - 1)
                     {
-                        npcDialogueBox.text = npc.dialogue[2];
+                        state++;
+                        curResponseTracker = 0;
+                        playerResponse.text = npcs[state].playerDialogue[0];
+
                     }
                 }
-                else if (curResponseTracker == 2 && npc.playerDialogue.Length >= 2)
+
+            }
+            else if (curResponseTracker == 1 && npc.playerDialogue.Length >= 1)
+            {
+                playerResponse.text = npc.playerDialogue[1];
+                if (Input.GetKeyDown(KeyCode.Return))
                 {
-                    playerResponse.text = npc.playerDialogue[2];
-                    if (Input.GetKeyDown(KeyCode.Return))
+                    int dialogue = Alice ? (npc.dialogue.Length / 2) + 2 : 2;
+                    npcDialogueBox.text = npc.dialogue[dialogue];
+                    if (state < npcs.Count - 1)
                     {
-                       npcDialogueBox.text = npc.dialogue[3];
+                        state++;
+                        curResponseTracker = 0;
+                        playerResponse.text = npcs[state].playerDialogue[0];
                     }
                 }
+            }
+            else if (curResponseTracker == 2 && npc.playerDialogue.Length >= 2)
+            {
+                playerResponse.text = npc.playerDialogue[2];
+                if (Input.GetKeyDown(KeyCode.Return))
+                {
+                    int dialogue = Alice ? (npc.dialogue.Length / 2) + 3 : 3;
+                    npcDialogueBox.text = npc.dialogue[dialogue];
+                    if (state < npcs.Count - 1)
+                    {
+                        state++;
+                        curResponseTracker = 0;
+                        playerResponse.text = npcs[state].playerDialogue[0];
+                    }
+                }
+            }
         }
-
-      }
-
-        void StartConversation()
+        else
         {
-            isTalking = true;
-            curResponseTracker = 0;
-            dialogueUI.SetActive(true);
-            npcName.text = npc.lapinName;
-            npcDialogueBox.text = npc.dialogue[0];
+            EndDialogue();
         }
+    }
 
-        void EndDialogue()
-        {
+    void StartConversation()
+    {
+        isTalking = true;
+        curResponseTracker = 0;
+        dialogueUI.SetActive(true);
+        npcName.text = npcs[state].lapinName;
+        npcDialogueBox.text = npcs[state].dialogue[0];
+    }
+
+    void EndDialogue()
+    {
         isTalking = false;
         dialogueUI.SetActive(false);
 
-        }
-
-
     }
+
+
+}
